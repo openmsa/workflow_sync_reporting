9cde65fec45850a97ba1730ff3a42e06e769128a
xiangyuzz <xiz@ubiqube.com>
2023-09-29 14:54:52 +0100

    hide some fileds

--- MSA_Sync_Report/Process_Generate_ES_Snapshot_Report/Tasks/Task_Get_Kibana_Credentials.py
+++ MSA_Sync_Report/Process_Generate_ES_Snapshot_Report/Tasks/Task_Get_Kibana_Credentials.py
@@ -3,11 +3,14 @@ from msa_sdk.msa_api import MSA_API
 
 if __name__ == "__main__":
     dev_var = Variables()
-    dev_var.add('kibana_ip', var_type='String')
-    dev_var.add('kibana_username', var_type='String')
+    #dev_var.add('kibana_ip', var_type='String')
+    #dev_var.add('kibana_username', var_type='String')
     dev_var.add('kibana_password', var_type='String')
 
     context = Variables.task_call(dev_var)
+    
+    context["kibana_username"]="superuser"
+    context["kibana_password"]="x^ZyuGM6~u=+fY2G"
     
     ret = MSA_API.process_content('ENDED', 'Instance created', context, True)
     print(ret)
